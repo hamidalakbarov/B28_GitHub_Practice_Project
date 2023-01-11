9c68335b812dc0ff60943a8f2002a7e856f65c03
Hamid <hamidalakbarov@gmail.com>
2023-01-11 10:25:50 -0800

    Revert "Changing file"
    This reverts commit 10119fd4bc97c7a8b3a81a760a1bfd4546a53eba.

--- src/cydeo/TestCase004.java
+++ src/cydeo/TestCase004.java
@@ -5,7 +5,5 @@ public class TestCase004 {
     public static void main(String[] args) {
 
         System.out.println("Created steps for my job");
-
-        System.out.println("made changes");
     }
 }
