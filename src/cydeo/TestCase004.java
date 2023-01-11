a7397b5c15c5b56fa2ddf96c80ba1560badc92aa
Hamid <hamidalakbarov@gmail.com>
2023-01-11 10:31:18 -0800

    Changing file

--- src/cydeo/TestCase004.java
+++ src/cydeo/TestCase004.java
@@ -5,5 +5,7 @@ public class TestCase004 {
     public static void main(String[] args) {
 
         System.out.println("Created steps for my job");
+
+        System.out.println("Changes");
     }
 }
